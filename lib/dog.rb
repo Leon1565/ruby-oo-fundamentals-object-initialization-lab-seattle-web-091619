07ebaf057bf4661fdcce21c1cff090fb610222ee
Cha Yang <chachaiyang3@gmail.com>
2019-09-17 22:53:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,4 @@ class Dog
   def initialize(name)
     @name = name
   end
-  
-  def initialize(breed)
-    @breed = breed
-  end
-  
-  def initialize(breed = "Mutt")
-    @breed = breed
-  end
 end
